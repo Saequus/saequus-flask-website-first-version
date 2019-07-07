4609f8ffc75fb5f47b8333b7e078d9e4a9630c70
Slava Spetsyian <slavaspetsyian@gmail.com>
2019-07-07 11:14:37 +0300

    Added uwsgi and heroku Procfile

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config.update(DEBUG=True, SECRET_KEY='secretkey',
+                  USERNAME='admin', PASSWORD='admin')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/slavaspetsyian'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 from webapp import routes
